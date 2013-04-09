e15bccc6c8a63101d2813604acdfee17d3b777f9
Taylor Savage <taylor@taylorsavage.com>
2013-04-08 18:12:00 -0700

    Created basic view

--- planit/settings.py
+++ planit/settings.py
@@ -1,4 +1,6 @@
 # Django settings for planit project.
+import os, sys
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -106,6 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
