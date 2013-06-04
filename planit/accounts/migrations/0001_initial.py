832d6cbbc42df207023e71c28d32cd436e979ea1
Taylor Savage <taylor@taylorsavage.com>
2013-06-03 22:07:05 -0700

    Cleaned up migrations

--- planit/accounts/migrations/0001_initial.py
+++ planit/accounts/migrations/0001_initial.py
@@ -13,29 +13,71 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
             ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('phone', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('phone_confirmed', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('phone', self.gf('phonenumber_field.modelfields.PhoneNumberField')(unique=True, max_length=128)),
+            ('phone_verified', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
             ('email', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('newly_created', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal(u'accounts', ['UserProfile'])
+
+        # Adding model 'LoginToken'
+        db.create_table(u'accounts_logintoken', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('phone', self.gf('phonenumber_field.modelfields.PhoneNumberField')(max_length=128)),
+            ('token', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('date_created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('next_url', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+        ))
+        db.send_create_signal(u'accounts', ['LoginToken'])
+
+        # Adding model 'VerificationToken'
+        db.create_table(u'accounts_verificationtoken', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['accounts.UserProfile'])),
+            ('token', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('date_created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'accounts', ['VerificationToken'])
 
 
     def backwards(self, orm):
         # Deleting model 'UserProfile'
         db.delete_table(u'accounts_userprofile')
 
+        # Deleting model 'LoginToken'
+        db.delete_table(u'accounts_logintoken')
+
+        # Deleting model 'VerificationToken'
+        db.delete_table(u'accounts_verificationtoken')
+
 
     models = {
+        u'accounts.logintoken': {
+            'Meta': {'object_name': 'LoginToken'},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'next_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'phone': ('phonenumber_field.modelfields.PhoneNumberField', [], {'max_length': '128'}),
+            'token': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
         u'accounts.userprofile': {
             'Meta': {'object_name': 'UserProfile'},
             'email': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'newly_created': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'phone': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'phone_confirmed': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+            'phone': ('phonenumber_field.modelfields.PhoneNumberField', [], {'unique': 'True', 'max_length': '128'}),
+            'phone_verified': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'accounts.verificationtoken': {
+            'Meta': {'object_name': 'VerificationToken'},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'token': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['accounts.UserProfile']"})
         }
     }
 
--- planit/scheduling/migrations/0001_initial.py
+++ planit/scheduling/migrations/0001_initial.py
@@ -19,10 +19,73 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal(u'scheduling', ['ScheduleBlock'])
 
+        # Adding model 'Meeting'
+        db.create_table(u'scheduling_meeting', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('range_start', self.gf('django.db.models.fields.DateTimeField')()),
+            ('range_end', self.gf('django.db.models.fields.DateTimeField')()),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(related_name='creator', to=orm['accounts.UserProfile'])),
+            ('set_time', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='set_time', null=True, to=orm['scheduling.SuggestedTime'])),
+            ('duration', self.gf('django.db.models.fields.IntegerField')()),
+        ))
+        db.send_create_signal(u'scheduling', ['Meeting'])
+
+        # Adding M2M table for field users on 'Meeting'
+        m2m_table_name = db.shorten_name(u'scheduling_meeting_users')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('meeting', models.ForeignKey(orm[u'scheduling.meeting'], null=False)),
+            ('userprofile', models.ForeignKey(orm[u'accounts.userprofile'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['meeting_id', 'userprofile_id'])
+
+        # Adding model 'SuggestedTime'
+        db.create_table(u'scheduling_suggestedtime', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('meeting', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['scheduling.Meeting'])),
+            ('datetime', self.gf('django.db.models.fields.DateTimeField')()),
+            ('set_as_time', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'scheduling', ['SuggestedTime'])
+
+        # Adding M2M table for field accepted on 'SuggestedTime'
+        m2m_table_name = db.shorten_name(u'scheduling_suggestedtime_accepted')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('suggestedtime', models.ForeignKey(orm[u'scheduling.suggestedtime'], null=False)),
+            ('userprofile', models.ForeignKey(orm[u'accounts.userprofile'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['suggestedtime_id', 'userprofile_id'])
+
+        # Adding M2M table for field declined on 'SuggestedTime'
+        m2m_table_name = db.shorten_name(u'scheduling_suggestedtime_declined')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('suggestedtime', models.ForeignKey(orm[u'scheduling.suggestedtime'], null=False)),
+            ('userprofile', models.ForeignKey(orm[u'accounts.userprofile'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['suggestedtime_id', 'userprofile_id'])
+
 
     def backwards(self, orm):
         # Deleting model 'ScheduleBlock'
         db.delete_table(u'scheduling_scheduleblock')
+
+        # Deleting model 'Meeting'
+        db.delete_table(u'scheduling_meeting')
+
+        # Removing M2M table for field users on 'Meeting'
+        db.delete_table(db.shorten_name(u'scheduling_meeting_users'))
+
+        # Deleting model 'SuggestedTime'
+        db.delete_table(u'scheduling_suggestedtime')
+
+        # Removing M2M table for field accepted on 'SuggestedTime'
+        db.delete_table(db.shorten_name(u'scheduling_suggestedtime_accepted'))
+
+        # Removing M2M table for field declined on 'SuggestedTime'
+        db.delete_table(db.shorten_name(u'scheduling_suggestedtime_declined'))
 
 
     models = {
@@ -32,9 +95,21 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'newly_created': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'phone': ('phonenumber_field.modelfields.PhoneNumberField', [], {'unique': 'True', 'max_length': '128'}),
             'phone_verified': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'scheduling.meeting': {
+            'Meta': {'object_name': 'Meeting'},
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'creator'", 'to': u"orm['accounts.UserProfile']"}),
+            'duration': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'range_end': ('django.db.models.fields.DateTimeField', [], {}),
+            'range_start': ('django.db.models.fields.DateTimeField', [], {}),
+            'set_time': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'set_time'", 'null': 'True', 'to': u"orm['scheduling.SuggestedTime']"}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'users'", 'symmetrical': 'False', 'to': u"orm['accounts.UserProfile']"})
         },
         u'scheduling.scheduleblock': {
             'Meta': {'object_name': 'ScheduleBlock'},
@@ -44,6 +119,15 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'start': ('django.db.models.fields.TimeField', [], {}),
             'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['accounts.UserProfile']"})
+        },
+        u'scheduling.suggestedtime': {
+            'Meta': {'object_name': 'SuggestedTime'},
+            'accepted': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'accepted'", 'symmetrical': 'False', 'to': u"orm['accounts.UserProfile']"}),
+            'datetime': ('django.db.models.fields.DateTimeField', [], {}),
+            'declined': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'declined'", 'symmetrical': 'False', 'to': u"orm['accounts.UserProfile']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'meeting': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['scheduling.Meeting']"}),
+            'set_as_time': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
         }
     }
 
